e7403ca7afaef196d0ea34cee51eaeef55b9f3ba
Milan van Wouden <milan.vanwouden@pttrns.ai>
2023-03-15 15:39:56 +0100

    use ij indexing for torch meshgrid

--- models/geometry.py
+++ models/geometry.py
@@ -44,7 +44,7 @@ class MarchingCubeHelper(nn.Module):
     def grid_vertices(self):
         if self.verts is None:
             x, y, z = torch.linspace(*self.points_range, self.resolution), torch.linspace(*self.points_range, self.resolution), torch.linspace(*self.points_range, self.resolution)
-            x, y, z = torch.meshgrid(x, y, z, indexing='xy')
+            x, y, z = torch.meshgrid(x, y, z, indexing='ij')
             verts = torch.cat([x.reshape(-1, 1), y.reshape(-1, 1), z.reshape(-1, 1)], dim=-1).reshape(-1, 3)
             self.verts = verts
         return self.verts
