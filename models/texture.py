3bf33830553daf7c776b1ab10034822a8d97d032
wangyida <yidawang.cn@gmail.com>
2023-06-27 10:28:59 +0800

    simplify appearance embedding for inference

--- models/texture.py
+++ models/texture.py
@@ -23,35 +23,29 @@ class VolumeRadiance(nn.Module):
         self.n_output_dims = 3
         encoding = get_encoding(self.n_dir_dims, self.config.dir_encoding_config)
         self.n_input_dims = self.config.input_feature_dim + encoding.n_output_dims
+        if self.config.use_appearance_embedding:
+            self.n_input_dims += self.config.appearance_embedding_dim
+            self.embedding_appearance = Embedding(self.config.num_images, self.config.appearance_embedding_dim)
         network = get_mlp(self.n_input_dims, self.n_output_dims, self.config.mlp_network_config)    
         self.encoding = encoding
         self.network = network
 
-        # image embedding
-        self.num_images = 379 # adjust it according to your dataset
-        self.embedding_appearance = Embedding(self.num_images, self.config.appearance_embedding_dim)
-        self.use_average_appearance_embedding = self.config.use_average_appearance_embedding
 
     
     def forward(self, features, dirs, camera_indices, ray_indices, *args):
         dirs = (dirs + 1.) / 2. # (-1, 1) => (0, 1)
         dirs_embd = self.encoding(dirs.view(-1, self.n_dir_dims))
-        # appearance
-        if self.training:
-            embedded_appearance = self.embedding_appearance(camera_indices)
-            # set it to zero if don't use it
-            if not self.config.use_appearance_embedding:
-                embedded_appearance = torch.zeros_like(embedded_appearance)
-        else:
-            if self.use_average_appearance_embedding:
-                embedded_appearance = torch.ones(
-                    (*dirs.shape[:-1], self.config.appearance_embedding_dim), device=dirs.device
-                ) * self.embedding_appearance.mean(dim=0)
+        # with appearance embeddings
+        if self.config.use_appearance_embedding:
+            if self.training:
+                appe_embd = self.embedding_appearance(camera_indices)
             else:
-                embedded_appearance = torch.zeros(
-                    (*dirs.shape[:-1], self.config.appearance_embedding_dim), device=dirs.device
-                )
-        network_inp = torch.cat([features.view(-1, features.shape[-1]), dirs_embd, embedded_appearance[ray_indices]] + [arg.view(-1, arg.shape[-1]) for arg in args], dim=-1)
+                appe_embd = self.embedding_appearance.mean(dim=0).repeat(features.size()[0], 1)
+                if not self.config.use_average_appearance_embedding:
+                    appe_embd *= 0
+            network_inp = torch.cat([features.view(-1, features.shape[-1]), dirs_embd, appe_embd[ray_indices]] + [arg.view(-1, arg.shape[-1]) for arg in args], dim=-1)
+        else:
+            network_inp = torch.cat([features.view(-1, features.shape[-1]), dirs_embd] + [arg.view(-1, arg.shape[-1]) for arg in args], dim=-1)
         color = self.network(network_inp).view(*features.shape[:-1], self.n_output_dims).float()
         if 'color_activation' in self.config:
             color = get_activation(self.config.color_activation)(color)
@@ -71,11 +65,24 @@ class VolumeColor(nn.Module):
         self.config = config
         self.n_output_dims = 3
         self.n_input_dims = self.config.input_feature_dim
+        if self.config.use_appearance_embedding:
+            self.n_input_dims += self.config.appearance_embedding_dim
+            self.embedding_appearance = Embedding(self.config.num_images, self.config.appearance_embedding_dim)
         network = get_mlp(self.n_input_dims, self.n_output_dims, self.config.mlp_network_config)
         self.network = network
     
-    def forward(self, features, *args):
-        network_inp = features.view(-1, features.shape[-1])
+    def forward(self, features, camera_indices, ray_indices, *args):
+        # with appearance embeddings
+        if self.config.use_appearance_embedding:
+            if self.training:
+                appe_embd = self.embedding_appearance(camera_indices)
+            else:
+                appe_embd = self.embedding_appearance.mean(dim=0).repeat(features.size()[0], 1)
+                if not self.config.use_average_appearance_embedding:
+                    appe_embd *= 0
+            network_inp = torch.cat([features.view(-1, features.shape[-1]), appe_embd[ray_indices]], dim=-1)
+        else:
+            network_inp = features.view(-1, features.shape[-1])
         color = self.network(network_inp).view(*features.shape[:-1], self.n_output_dims).float()
         if 'color_activation' in self.config:
             color = get_activation(self.config.color_activation)(color)
